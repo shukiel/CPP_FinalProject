2337a6f36d061d46e2664f36a6407316c8bf4aa3
Aviad Zini <aviadzini@gmail.com>
2016-05-03 22:15:56 +0300

    Crew cpp+h

---
+++ Final Project C++/Crew.cpp
@@ -0,0 +1,17 @@
+#include "Crew.h"
+
+istream& operator>>(istream& in, Crew& crew)
+{
+	in >> (Employee&) crew >> crew.m_numOfBeersDrank;
+	return in;
+}
+
+void Crew::toOs(ostream& os)
+{
+	os << "Num of beers drank: " << m_numOfBeersDrank << endl;
+}
+
+void Crew::soberUp()
+{
+	m_numOfBeersDrank = 0;
+}
--- Final Project C++/Crew.h
+++ Final Project C++/Crew.h
@@ -9,15 +9,16 @@ private:
 	int m_numOfBeersDrank;
 
 public:
-	Crew(const Employee& other, int numOfBeersDrank = 0);
-	Crew(const char* name, const char* phoneNum, const char* address, int m_salaryPerHour);
+	Crew(const Employee& other, int numOfBeersDrank = 0) : Employee(other), m_numOfBeersDrank(numOfBeersDrank) { }
+	//Crew(const char* name, const char* phoneNum, const char* address, int m_salaryPerHour);
 
-	void setNumOfBeersDrank(int numOfBeersDrank) { this->m_numOfBeersDrank = numOfBeersDrank; }
+	void setNumOfBeersDrank(int numOfBeersDrank) { m_numOfBeersDrank = numOfBeersDrank; }
 
 	int getNumOfBeersDrank() const { return m_numOfBeersDrank; }
 
-	friend ostream& operator<<(ostream& os, const Crew& crew);
 	friend istream& operator>>(istream& in, Crew& crew);
+
+	virtual void toOs(ostream& os);
 
 	void soberUp();
 };
